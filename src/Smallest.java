487200dc500d23e26626dbaf559dfd9edbd01cb6
AbhayGoyal001 <abhaygoyalscience@gmail.com>
2025-01-17 14:21:32 +0530

    Changing Variable Name

--- src/Smallest.java
+++ src/Smallest.java
@@ -7,9 +7,9 @@ public class Smallest {
         System.out.print("Enter three numbers: ");
         int a = scanner.nextInt();
         int b = scanner.nextInt();
-        int d = scanner.nextInt();
+        int e = scanner.nextInt();
 
-        int smallest = Math.min(a, Math.min(b, d));
+        int smallest = Math.min(a, Math.min(b, e));
         System.out.println("The smallest number is: " + smallest);
 
         scanner.close();
